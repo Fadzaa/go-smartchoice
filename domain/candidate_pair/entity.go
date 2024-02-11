372e71c044334a914d051a811ad284fa3f69025c
Fadzaa <fadza20@gmail.com>
2024-02-11 16:05:47 +0700

    fix entities

--- domain/candidate_pair/entity.go
+++ domain/candidate_pair/entity.go
@@ -8,10 +8,12 @@ type CandidatePair struct {
 	ID                  uint          `json:"id" gorm:"primaryKey"`
 	PairName            string        `json:"pair_name" gorm:"maxLength:100"`
 	PairSlogan          string        `json:"pair_slogan" gorm:"maxLength:100"`
-	PresCandidate       string        `json:"pres_candidate" gorm:"maxLength:100"`
 	PresId              uint          `json:"pres_id"`
-	ViceCandidate       string        `json:"vice_candidate" gorm:"maxLength:100"`
+	PresCandidate       string        `json:"pres_candidate" gorm:"maxLength:100"`
+	PresImage           string        `json:"pres_image"`
 	ViceId              uint          `json:"vice_id"`
+	ViceCandidate       string        `json:"vice_candidate" gorm:"maxLength:100"`
+	ViceImage           string        `json:"vice_image"`
 	VisionMissionLink   string        `json:"vision_mission_link"`
 	Education           string        `json:"education"`
 	Health              string        `json:"health"`
--- domain/debate/entity.go
+++ domain/debate/entity.go
@@ -1,13 +1,8 @@
 package debate
 
-import (
-	"gorm.io/gorm"
-)
-
 type Debate struct {
-	gorm.Model
+	ID          uint   `json:"id" gorm:"primaryKey"`
 	Name        string `json:"name" gorm:"maxLength:100"`
-	Image       string `json:"image"`
 	Description string `json:"description" gorm:"maxLength:100"`
 	Date        string `json:"date" gorm:"maxLength:100"`
 	Link        string `json:"link"`
