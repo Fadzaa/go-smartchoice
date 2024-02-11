a3b3b276112bfdb8d76faa8fc04dd767793c1507
Fadzaa <fadza20@gmail.com>
2024-02-11 16:13:27 +0700

    fix port

--- main.go
+++ main.go
@@ -13,7 +13,11 @@ func main() {
 	router := InitializedApp()
 
 	port := os.Getenv("PORT")
-	err := router.Run(port)
+
+	if port == "" {
+		port = "8080"
+	}
+	err := router.Run("0.0.0.0:" + port)
 	if err != nil {
 		panic(err)
 	}
